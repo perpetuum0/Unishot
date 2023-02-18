63ca7b3978f45bb9fe58e5d8b7a17f9a649bb966
perpetuum0 <serge1snnnn@gmail.com>
2023-02-18 22:02:02 +0600

    Implement drawing text, Draw color.

--- src/drawing.py
+++ src/drawing.py
@@ -1,9 +1,26 @@
-from PySide6.QtWidgets import QWidget, QLabel
-from PySide6.QtCore import Qt, QRect, QPoint, QLineF
-from PySide6.QtGui import QMouseEvent, QPainter, QPixmap, QPainterPath, QPen, QShortcut, QKeySequence
+from PySide6.QtWidgets import QWidget, QLabel, QTextEdit
+from PySide6.QtCore import Qt, QRect, QPoint, QLineF, Signal, QSize
+from PySide6.QtGui import QMouseEvent, QPainter, QPixmap, QPainterPath, QPen, QShortcut, QKeySequence, QColor
 
 from utils import mapPointToRect
 from typings import DrawTools, Drawing
+
+
+class DrawTextEdit(QTextEdit):
+    lostFocus = Signal()
+
+    def __init__(self, parent: QWidget, color=QColor("red")):
+        super().__init__(parent)
+        self.setStyleSheet("background-color: rgba(0,0,0,0); border: none")
+        self.setHorizontalScrollBarPolicy(
+            Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(
+            Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.setTextColor(color)
+
+    def focusOutEvent(self, e) -> None:
+        self.lostFocus.emit()
+        e.accept()
 
 
 class Draw(QLabel):
@@ -11,22 +28,33 @@ class Draw(QLabel):
     attribute = Qt.WidgetAttribute.WA_TransparentForMouseEvents
 
     tool: Tools
+    color: QColor
     preview: QPixmap
     drawings: list[Drawing]
+    textEdit: QTextEdit
 
+    editingText: bool
     newDrawing: bool
     brushPath: QPainterPath
 
     def __init__(self, parent: QWidget):
         super().__init__(parent)
+        self.textEdit = DrawTextEdit(self)
+        self.textEdit.hide()
+        self.textEdit.lostFocus.connect(self.stopTextEdit)
+        self.editingText = False
+
         self.undoShortcut = QShortcut(QKeySequence.StandardKey.Undo, self)
         self.undoShortcut.activated.connect(self.undo)
+
         self.setCursor(Qt.CursorShape.UpArrowCursor)  # Debug
         self.setTransparent(True)
 
-    def start(self, tool: Tools, width: int = 5):
+    def start(self, tool: Tools, color=QColor("red"), width: int = 5):
+        self.textEdit.hide()
         self.setTransparent(False)
         self.tool = tool
+        self.color = color
         self.penWidth = width
         self.brushPoints = []
 
@@ -36,12 +64,18 @@ class Draw(QLabel):
     def setCanvas(self, rect: QRect):
         self.drawings = []
         self.setGeometry(rect)
+        self.textEdit.resize(rect.size())
         self.updatePreview()
 
     def mousePressEvent(self, event: QMouseEvent) -> None:
+        if self.editingText:
+            self.stopTextEdit()
+
         self.startPoint = event.globalPos()
         self.brushPath = QPainterPath(self.startPoint)
         self.newDrawing = True
+        self.editingText = True
+
         event.accept()
 
     def mouseMoveEvent(self, event: QMouseEvent) -> None:
@@ -80,12 +114,12 @@ class Draw(QLabel):
 
     def getDrawing(self) -> Drawing:
         margin = self.penWidth  # Prevent cropping drawings
-        tmp = QRect(self.startPoint, self.endPoint).normalized()
+        normalized = QRect(self.startPoint, self.endPoint).normalized()
         selectionRect = QRect(
-            QPoint(tmp.topLeft().x()-margin,
-                   tmp.topLeft().y()-margin),
-            QPoint(tmp.bottomRight().x()+margin,
-                   tmp.bottomRight().y()+margin)
+            QPoint(normalized.topLeft().x()-margin,
+                   normalized.topLeft().y()-margin),
+            QPoint(normalized.bottomRight().x()+margin,
+                   normalized.bottomRight().y()+margin)
         ) if not self.tool is self.Tools.Brush \
             else self.geometry()
 
@@ -99,7 +133,7 @@ class Draw(QLabel):
         painter = QPainter(pixmap)
         painter.setRenderHint(QPainter.RenderHint.Antialiasing)
 
-        pen = QPen("red")
+        pen = QPen(self.color)
         pen.setWidth(self.penWidth)
         painter.setPen(pen)
 
@@ -126,11 +160,24 @@ class Draw(QLabel):
             case self.Tools.Line:
                 painter.drawLine(localStartPoint, localEndPoint)
             case self.Tools.Text:
-                painter.drawText(
-                    localRect, "Hello, world! To be implemented."
-                )
+                if self.editingText:
+                    self.startTextEdit(selectionRect.topLeft())
+                else:
+                    painter.drawText(localRect, self.textEdit.toPlainText())
 
         return Drawing(selectionRect, pixmap)
+
+    def startTextEdit(self, position: QPoint):
+        self.textEdit.move(position)
+        self.textEdit.show()
+        self.textEdit.setFocus()
+
+    def stopTextEdit(self):
+        self.editingText = False
+        self.doDrawing()
+
+        self.textEdit.hide()
+        self.textEdit.clear()
 
     def undo(self) -> None:
         try:
